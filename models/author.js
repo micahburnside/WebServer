06ae4cc24f43d152319ab46765dd3947367938a6
Micah Burnside <micahburnside@protonmail.com>
2024-08-06 09:26:49 -0400

    Fix author schema and update new author form

--- models/author.js
+++ models/author.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose')
 
 const authorSchema = new mongoose.Schema({
-  name: String,
-  required: true
+  name: {
+    type: String,
+    required: true
+  }
 })
 
 module.exports = mongoose.model('Author', authorSchema)
