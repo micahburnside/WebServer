08378c3a3d91ebcd05b6301a8bf08b90f9b81623
Micah Burnside <micahburnside@protonmail.com>
2024-08-06 14:40:19 -0400

    Fetch and display authors on the authors index page

--- routes/authors.js
+++ routes/authors.js
@@ -4,7 +4,12 @@ const Author = require('../models/author')
 
 // All Authors Route
 router.get('/', (req, res) => {
-  res.render('authors/index')
+  try {
+    const authors = await Author.find({})
+    res.render('authors/index', {authors: authors})
+  } catch {
+    res.redirect('/')
+  }
 })
 
 // New Author Route
