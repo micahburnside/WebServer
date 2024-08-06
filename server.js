39176668420c5a75676b566945e0f135306cb53a
Micah Burnside <micahburnside@protonmail.com>
2024-08-05 22:46:58 -0400

    Add index router to server

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 
+const indexRouter = require('./routes/index')
+
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
