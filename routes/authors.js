4bca78cf3a2278bd48e486b7e9984c0f121e1bfd
Micah Burnside <micahburnside@protonmail.com>
2024-08-06 15:03:32 -0400

    Add author search functionality

--- routes/authors.js
+++ routes/authors.js
@@ -1,20 +1,23 @@
 const express = require('express')
 const router = express.Router()
 const Author = require('../models/author')
+const {query} = require("express");
 
 // All Authors Route
 router.get('/', async (req, res) => {
-
+let searchOptions = {}
+  if(req.query.name != null && query.name !== ''){
+    searchOptions.name = new RegExp(req.query.name, 'i')
+  }
   let authors = [];
-
   try {
-    authors = await Author.find({})
-  } catch {}
-
-  res.render('authors/index', {
-    authors: authors,
-    errorMessage: null,
-  })
+    authors = await Author.find(searchOptions)
+    res.render('authors/index', {
+      authors: authors,
+      searchOptions: req.query})
+  } catch {
+    res.redirect('/')
+  }
 })
 
 // New Author Route
