da8f3e3412fb1d22ff5d21bc6f3f919ca7b7130e
Micah Burnside <micahburnside@protonmail.com>
2024-08-06 19:41:54 -0400

    Replace placeholder responses in book routes

--- routes/books.js
+++ routes/books.js
@@ -5,17 +5,17 @@ const {query} = require("express");
 
 // All Books Route
 router.get('/', async (req, res) => {
-
+  res.send('All Books')
 })
 
 // New Book Route
 router.get('/new', (req, res) => {
-
+  res.send('New Book')
 })
 
 // Create Book Route
 router.post('/', async (req, res) => {
-
+  res.send('Create Book')
 })
 
 module.exports = router
