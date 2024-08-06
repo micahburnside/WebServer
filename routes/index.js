5552805c53c9cc01d21d1aa6c8cbb5826f61a38c
Micah Burnside <micahburnside@protonmail.com>
2024-08-05 23:10:29 -0400

    Update route handler and rename layout file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const res = require("express/lib/response");
+// const res = require("express/lib/response");
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('index')
 })
 
 module.exports = router
+
