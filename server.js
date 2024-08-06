5d16a3ba259904846fecf1c145dba603237a3cae
Micah Burnside <micahburnside@protonmail.com>
2024-08-06 16:06:17 -0400

    Configure dotenv handling and enhance server setup.

--- server.js
+++ server.js
@@ -1,33 +1,37 @@
 if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
+  require('dotenv').config();
 }
-const express = require('express')
-const app = express()
-const expressLayouts = require('express-ejs-layouts')
-// const dotenv = require('dotenv').config()
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
-const bodyParser = require('body-parser')
+const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
 
-const indexRouter = require('./routes/index')
-const authorRouter = require('./routes/authors')
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+app.use(expressLayouts);
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 
-app.set('view engine', 'ejs')
-app.set('views', __dirname + '/views')
-app.set('layout', 'layouts/layout')
-app.use(expressLayouts)
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
+console.log("Database URL: ", process.env.DATABASE_URL);
 
-console.log("Database URL: ", process.env.DATABASE_URL)
+const mongoose = require('mongoose');
 
-const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
-const db = mongoose.connection
-db.on('error', (err) => console.error(err))
-db.once('open', (err) => console.log('Connected to Mongoose'))
+const isProduction = process.env.NODE_ENV === 'production';
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  ssl: isProduction, // Enable SSL only in production
+});
 
-app.use('/', indexRouter)
-app.use('/authors', authorRouter)
+const db = mongoose.connection;
+db.on('error', (err) => console.error(err));
+db.once('open', (err) => console.log('Connected to Mongoose'));
 
-app.listen(process.env.PORT || 3000, () => console.log('Server Started'))
+app.use('/', indexRouter);
+app.use('/authors', authorRouter);
+
+app.listen(process.env.PORT || 3000, () => console.log('Server Started'));
