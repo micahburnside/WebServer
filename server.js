72660f9bea5b062f0a8c756c4f45b2c503d1f4ce
Micah Burnside <micahburnside@protonmail.com>
2024-08-05 22:35:02 -0400

    Add express layouts to the server

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ const expressLayouts = require('express-ejs-layouts')
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
-
+app.use(expressLayouts)
