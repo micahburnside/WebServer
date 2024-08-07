bc4b5332f80a9608143a649b40707fea745ede50
Micah Burnside <micahburnside@protonmail.com>
2024-08-06 21:26:16 -0400

    Add image upload functionality for books with error handling

--- routes/books.js
+++ routes/books.js
@@ -1,7 +1,18 @@
 const express = require('express')
 const router = express.Router()
+const multer = require('multer')
+const path = require('path')
+const fs = require('fs')
 const Book = require('../models/book')
+const uploadPath = path.join('public', Book.coverImageBasePath)
 const Author = require('../models/author')
+const imageMimeTypes = ['images/jpeg', 'images/png', 'images/gif']
+const upload = multer({
+  dest: uploadPath,
+  fileFilter: (req, file, callback) => {
+    callback(null, true)
+  }
+})
 const {query} = require("express");
 
 // All Books Route
@@ -11,27 +22,50 @@ router.get('/', async (req, res) => {
 
 // New Book Route
 router.get('/new', async (req, res) => {
-  try {
-    const authors = await Author.find({})
-    const book = new Book()
-    res.render('books/new', {
-      authors: authors,
-      book: book
-    })
-  } catch {
-    res.redirect('/books')
-  }
+  renderNewPage(res, new Book())
 })
 
 // Create Book Route
-router.post('/', async (req, res) => {
+router.post('/', upload.single('cover'), async (req, res) => {
+  const fileName = req.file != null ? req.file.filename : null
   const book = new Book({
     title: req.body.title,
     author: req.body.author,
     publishDate: new Date(req.body.publishDate),
     pageCount: req.body.pageCount,
+    coverImageName: fileName,
     description: req.body.description
   })
+  try {
+    const newBook = await book.save()
+    // res.redirect(`books/${newBook.id}`)
+    res.redirect(`books`)
+  } catch {
+    if(book.coverImageName != null) {
+      removeBookCover(book.coverImageName)
+    }
+    renderNewPage(res, book, true)
+  }
 })
+
+function removeBookCover (fileName) {
+  fs.unlinkSync(path.join(uploadPath, fileName), err => {
+    if (err) console.error(err)
+  })
+}
+
+async function renderNewPage(res, book, hasError = false) {
+  try {
+    const authors = await Author.find({})
+    const params = {
+      authors: authors,
+      book: book
+    }
+    if (hasError) params.errorMessage = 'Error Creating Book'
+    res.render('books/new', params)
+  } catch {
+    res.redirect('/books')
+  }
+}
 
 module.exports = router
