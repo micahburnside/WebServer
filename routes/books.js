aaecd5ebdba2a19711c96ea77400c1e696be21c2
Micah Burnside <micahburnside@protonmail.com>
2024-08-06 20:25:39 -0400

    Fix book model reference and add author selection in new book form

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
-const Book = require('../models/author')
+const Book = require('../models/book')
+const Author = require('../models/author')
 const {query} = require("express");
 
 // All Books Route
@@ -11,9 +12,14 @@ router.get('/', async (req, res) => {
 // New Book Route
 router.get('/new', async (req, res) => {
   try {
-
+    const authors = await Author.find({})
+    const book = new Book()
+    res.render('books/new', {
+      authors: authors,
+      book: book
+    })
   } catch {
-
+    res.redirect('/books')
   }
 })
 
