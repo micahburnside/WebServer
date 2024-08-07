d3e58601d14265c4ee5bfc38fdf41d8d37e2538c
Micah Burnside <micahburnside@protonmail.com>
2024-08-06 20:45:55 -0400

    Implement book creation in POST route

--- routes/books.js
+++ routes/books.js
@@ -25,7 +25,13 @@ router.get('/new', async (req, res) => {
 
 // Create Book Route
 router.post('/', async (req, res) => {
-  res.send('Create Book')
+  const book = new Book({
+    title: req.body.title,
+    author: req.body.author,
+    publishDate: new Date(req.body.publishDate),
+    pageCount: req.body.pageCount,
+    description: req.body.description
+  })
 })
 
 module.exports = router
