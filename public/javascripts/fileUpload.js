e73ec85ece2e0a02e8dff3fbc8ae98f3e7ae3750
Micah Burnside <micahburnside@protonmail.com>
2024-08-08 13:28:20 -0400

    Add FilePond configuration options

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -5,4 +5,10 @@ FilePond.registerPlugin(
     FilePondPluginFileEncode
 );
 
+FilePond.setOptions({
+    stylePanelAspectRatio: 150 /100,
+    imageResizeTargetWidth: 100,
+    imageResizeTargetHeight: 150
+})
+
 FilePond.parse(document.body);
