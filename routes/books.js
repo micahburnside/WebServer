f899906dfe424692ea005de89bb1dfbb5fea9688
Micah Burnside <micahburnside@protonmail.com>
2024-08-08 13:38:52 -0400

    Remove file upload functionality from book creation route

--- routes/books.js
+++ routes/books.js
@@ -45,7 +45,7 @@ router.get('/new', async (req, res) => {
 })
 
 // Create Book Route
-router.post('/', upload.single('cover'), async (req, res) => {
+router.post('/', async (req, res) => {
   const fileName = req.file != null ? req.file.filename : null
   const book = new Book({
     title: req.body.title,
