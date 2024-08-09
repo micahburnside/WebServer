603f75028ef99028ce5ae2f6ea204b7237321bbd
Micah Burnside <micahburnside@protonmail.com>
2024-08-09 15:32:01 -0400

    Implement author deletion with async handler

--- routes/authors.js
+++ routes/authors.js
@@ -72,8 +72,19 @@ router.put('/:id', async (req, res) => {
   }
 })
 
-router.delete('/:id',  (req, res) => {
-  res.send('Delete Author' + req.params.id)
+router.delete('/:id',  async (req, res) => {
+  let author
+  try {
+    author = await Author.findById(req.params.id)
+    await author.remove()
+    res.redirect('/authors')
+  } catch {
+    if (author == null) {
+      res.redirect('/')
+    } else {
+      res.redirect(`/authors/${author.id}`)
+    }
+  }
 })
 
 module.exports = router
