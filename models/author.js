187a0a921845984905fafbb58749fd8fe77734c0
Micah Burnside <micahburnside@protonmail.com>
2024-08-12 20:04:32 -0400

    Update README.md

--- models/author.js
+++ models/author.js
@@ -1,6 +1,18 @@
 const mongoose = require('mongoose')
 const Book = require('./book')
 
+const checkAuthorHasBooks = function (next) {
+  Book.find({author: this.id}, (err, books) => {
+    if (err) {
+      next(err);
+    } else if (books.length > 0) {
+      next(new Error('This author has books still'));
+    } else {
+      next();
+    }
+  });
+};
+
 const authorSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -8,16 +20,6 @@ const authorSchema = new mongoose.Schema({
   }
 })
 
-authorSchema.pre('remove', function(next) {
-  Book.find({ author: this.id }, (err, books) => {
-    if (err) {
-      next(err)
-    } else if (books.length > 0) {
-      next(new Error('This author has books still'))
-    } else {
-      next()
-    }
-  })
-})
+authorSchema.pre('remove', checkAuthorHasBooks);
 
-module.exports = mongoose.model('Author', authorSchema)
+module.exports = mongoose.model('Author', authorSchema);
